6fa15ecbd87a45a594757aaff71c4308e41f4ee3
angelargd8 <100092195+angelargd8@users.noreply.github.com>
2024-09-09 08:36:31 -0600

    feat(add): pandas

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import os
 from PIL import Image
 import pytesseract
+import cv2
+import pandas as pd
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 #ruta a la carpeta de imagenes
-folder1 = './src/pictures1'
+folder2 = './src/pictures2'
 
 def cargarImagenes(folder): 
     #lista de imagenes
@@ -17,7 +19,8 @@ def cargarImagenes(folder):
         extension = os.path.splitext(file)[1].lower()
         if extension in valid_extensions: 
             picturePath = os.path.join(folder, file)
-            picture = Image.open(picturePath)
+            #picture = Image.open(picturePath)
+            picture = cv2.imread(picturePath)
             pictures.append(picture) #guardar la imagen
     
     print("la cantidad de imagenes cargadas es: ", len(pictures))
@@ -27,9 +30,25 @@ def cargarImagenes(folder):
 
 def readPicture(pictures): 
     for picture in pictures: 
-        print("imagen " + str(pictures.index(picture)+1))
-        result = pytesseract.image_to_string(picture)
-        print(result)
+        #convertir a escala de grises
+        picture = cv2.cvtColor(picture, cv2.COLOR_BGR2GRAY)
+        #aplicar umbral
+        #_, picture = cv2.threshold(picture, 150, 255, cv2.THRESH_BINARY)
+        text = pytesseract.image_to_string(picture, lang='spa')
+        print(text)
+        #print("imagen " + str(pictures.index(picture)+1))
+        #result = pytesseract.image_to_string(picture)
+        #print(result)
+        return text
 
-pictures = cargarImagenes(folder1)
-readPicture(pictures)
+pictures = cargarImagenes(folder2)
+resultado = readPicture(pictures)
+
+#convertir los datos
+data = resultado.split('\n')
+data = [d.split() for d in data]
+print("datos: ")
+#print(data)
+data = pd.DataFrame(data)
+print(data)
+data.to_csv('data.csv', index=False, header=False)
